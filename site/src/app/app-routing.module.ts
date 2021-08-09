e15d892dda9d07eea36f2d4c71cc69b507a42157
jjovasevic <jovasevicjul@gmail.com>
2021-08-09 23:22:07 +0200

    Dodati opisi aprtmana

--- site/src/app/app-routing.module.ts
+++ site/src/app/app-routing.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AboutComponent } from './about/about.component';
 import { GalleryComponent } from './gallery/gallery.component';
 import { HomepageComponent } from './homepage/homepage.component';
-import { ProductsComponent } from './products/products.component';
 import {ApartmentsComponent} from './apartments/apartments.component';
 import {PricesComponent} from './prices/prices.component';
 import { ContactComponent } from './contact/contact.component';
@@ -13,7 +11,6 @@ const routes: Routes = [
   { path: 'gallery/:id', component: GalleryComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'apartments', component: ApartmentsComponent},
-  { path: 'products', component: ProductsComponent },
   { path: 'prices', component: PricesComponent}
 ];
 
